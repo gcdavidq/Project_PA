1a0c566065e522c3e420850dc882f4cdc5bba696
Antony16 <136536376+Sh3ccid@users.noreply.github.com>
2023-12-05 21:58:22 -0500

    Update analisis_departamental.py
    Signed-off-by: Antony16 <136536376+Sh3ccid@users.noreply.github.com>

--- analisis_departamental.py
+++ analisis_departamental.py
@@ -5,12 +5,11 @@ import folium
 from shapely.geometry import Point
 from streamlit_folium import folium_static
 import plotly.express as px
-
 @st.cache_data
 def load_department_boundaries():
     peru_departments = gpd.read_file('departamentos_perú.geojson')
     return peru_departments
-    
+
 @st.cache_data
 def load_data(file):
     data = pd.read_csv(file)
@@ -28,31 +27,80 @@ def assign_departments(data, department_boundaries):
 
 def create_map(data):
     if not data.empty:
-        st.subheader('Mapa de Puntos')
-        m = folium.Map(location=[-9.1900, -75.0152], zoom_start=6)
+        st.subheader('Mapa de Puntos y Gráfico de Barras')
 
+        # Asignar colores oscuros a cada departamento
+        department_colors = px.colors.qualitative.Dark24[:len(data['DEPARTAMENTOS'].unique())]
+        department_color_dict = dict(zip(data['DEPARTAMENTOS'].unique(), department_colors))
+
+        # Normalizar la magnitud y la profundidad para el mapa
+        data['normalized_magnitude'] = data['MAGNITUD'] / data['MAGNITUD'].max()
+        data['normalized_depth'] = data['PROFUNDIDAD'] / data['PROFUNDIDAD'].max()
+
+        # Mapa de puntos
+        m = folium.Map(location=[-9.1900, -75.0152], zoom_start=6)
         for index, row in data.iterrows():
-            folium.Marker([row['LATITUD'], row['LONGITUD']]).add_to(m)
+            # Calcular el color en función de la magnitud y profundidad
+            color = f'rgba(0, 0, 0, {row["normalized_magnitude"] * 0.8})'
+            fill_opacity = row['normalized_depth']
+
+            # Asignar color oscuro a cada departamento
+            color = department_color_dict[row['DEPARTAMENTOS']]
+
+            # Crear el círculo con información de magnitud y profundidad en el tooltip
+            folium.CircleMarker(
+                location=[row['LATITUD'], row['LONGITUD']],
+                radius=10,
+                color=color,
+                fill=True,
+                fill_opacity=fill_opacity,
+                tooltip=f'Magnitud: {row["MAGNITUD"]}, Profundidad: {row["PROFUNDIDAD"]}'
+            ).add_to(m)
 
         folium_static(m)
+
+        # Gráfico de barras
+        st.subheader('Cantidad de Sismos por DEPARTAMENTOS')
+        fig = px.bar(data, x='DEPARTAMENTOS', color='DEPARTAMENTOS', title='Cantidad de Sismos por Departamento',
+                     labels={'DEPARTAMENTOS': 'Departamento', 'count': 'Cantidad'},
+                     color_discrete_map=department_color_dict)
+
+        # Ajustar dinámicamente el dtick basado en la cantidad de datos
+        dtick_value = max(1, int(len(data) / 10))
+        fig.update_layout(xaxis_title='Departamento', yaxis_title='Cantidad de Sismos', yaxis=dict(dtick=dtick_value))
+        st.plotly_chart(fig)
+
     else:
-        st.info("No hay datos disponibles para mostrar en el mapa.")
+        st.info("No hay datos disponibles para mostrar en el mapa y el gráfico de barras.")
 
 def show_departments_count(data):
     st.subheader('Mapa Sísmico del Perú periodo 1960-2022 (IGP) ')
 
     data = data.rename(columns={'NOMBDEP': 'DEPARTAMENTOS'})
-    options = ['DEPARTAMENTOS','GRÁFICOS DE LINEA']
+    options = ['DEPARTAMENTOS', 'GRÁFICOS DE LINEA']
 
     selected_column = st.selectbox('Selecciona una columna:', options)
-    default_department = data[selected_column].unique()[0]
-    selected_values = st.multiselect(f'Selecciona valor(es) de {selected_column}:', data[selected_column].unique(),
-                                     default=default_department)
 
-    # Filter data based on the selected column and value(s)
-    filtered_data = data[data[selected_column].isin(selected_values)]
+    # Nueva lógica para la selección de valores (todos o específicos)
+    all_values_option = 'Todos'
+    unique_values = data[selected_column].unique()
+    selected_values = st.multiselect(f'Selecciona valor(es) de {selected_column}:',
+                                     options=[all_values_option] + list(unique_values),
+                                     default=[all_values_option])
 
-    # Filter data based on the selected years using the slider
+    # Verificar si la opción 'Todos' está seleccionada de forma independiente
+    select_all = all_values_option in selected_values
+    if select_all:
+        selected_values = [all_values_option]  # Restablecer la selección para incluir todos los valores
+
+    # Filtrar data en función de la selección
+    if select_all:
+        filtered_data = data
+    else:
+        filtered_data = data[data[selected_column].isin(selected_values)]
+
+
+    # Código restante para el slider y el mapa
     min_year = filtered_data['Año'].astype(int).min()
     max_year = filtered_data['Año'].astype(int).max()
 
@@ -63,21 +111,10 @@ def show_departments_count(data):
                                value=(min_year, max_year),
                                key="select_slider_years")
 
-    # Apply the year filter
+    # Aplicar el filtro de año
     filtered_data = filtered_data[(filtered_data['Año'].astype(int) >= selected_years[0]) &
                                   (filtered_data['Año'].astype(int) <= selected_years[1])]
 
     department_count = filtered_data.shape[0]
     st.write(f'En {selected_column} hay {department_count} puntos para los años {selected_years[0]} - {selected_years[1]}.')
     create_map(filtered_data)
-
-    # Gráfico de barras con intervalo dinámico en el eje y
-    st.subheader(f'Cantidad de Sismos por {selected_column}')
-    fig = px.bar(filtered_data[selected_column].value_counts(),
-                 x=filtered_data[selected_column].value_counts().index,
-                 y=filtered_data[selected_column].value_counts().values)
-
-    # Ajustar dinámicamente el dtick basado en la cantidad de datos
-    dtick_value = max(1, int(department_count / 10))  # Puedes ajustar el divisor según tus preferencias
-    fig.update_layout(xaxis_title=selected_column, yaxis_title=f'Cantidad de Sismos en {selected_column}', yaxis=dict(dtick=dtick_value))
-    st.plotly_chart(fig)
