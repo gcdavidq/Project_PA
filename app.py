b5e25faf157bf68f04137872940f260516500d77
Antony16 <136536376+Sh3ccid@users.noreply.github.com>
2023-12-05 23:17:22 -0500

    Update app.py
    Signed-off-by: Antony16 <136536376+Sh3ccid@users.noreply.github.com>

--- app.py
+++ app.py
@@ -19,7 +19,12 @@ page_bg_img = f"""
 """
 st.markdown(page_bg_img, unsafe_allow_html=True)
 
-image1 = Image.open('image1.png')
+video_file = st.file_uploader("p1", type=["mp4"])
+
+# Mostrar video si se ha cargado uno
+if video_file is not None:
+    video_url = f"data:video/{video_file.type};base64,{video_file.read().encode('base64').decode()}"
+    st.markdown(f'<video autoplay loop muted playsinline width="100%" height="auto"><source src="{video_url}" type="video/{video_file.type}"></video>', unsafe_allow_html=True)
 
 # Añadimos un panel de control
 tab1, tab2, tab3 = st.tabs([  "Inicio", "Análisis a nivel nacional", "Anális a nivel departamental"])
