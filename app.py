1f010eae3b7e716f613b441c226c99617f922e1e
Gian Quezada <150297069+gcdavidq@users.noreply.github.com>
2023-12-06 09:01:55 -0500

    Update app.py
    Signed-off-by: Gian Quezada <150297069+gcdavidq@users.noreply.github.com>

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ with tab1:
 # Análisis a nivel nacional
 with tab2:
     visualizacion_a_nivel_nacional("Catalogo1960_2022.csv")
-    mostrar_dashboard("Catalogo1960_2022.csv")
+    mostrar_dashboard("archivo_excel")
 
 
 
