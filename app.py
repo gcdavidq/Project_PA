d2fcee43aa546ad3337016dfb98acd8ba43c27c2
Marlith08 <150297300+Marlith08@users.noreply.github.com>
2023-12-06 06:11:33 -0500

    Update app.py
    Signed-off-by: Marlith08 <150297300+Marlith08@users.noreply.github.com>

--- app.py
+++ app.py
@@ -26,8 +26,8 @@ image1 = Image.open('Img_3.jpeg')
 tab1, tab2, tab3 = st.tabs(["_Streamlit_ is :blue[_Inicio_] :sunglasses:", "Análisis a nivel nacional", "Anális a nivel departamental"])
 
 with tab1:
-    font_style = 'forte'  # Puedes usar 'italic', 'oblique', 'normal', 'bold', etc.
-    color = 'blue'  # Puedes usar nombres de colores o códigos hexadecimales
+    font_style = 'showcard gothic'  # Puedes usar 'italic', 'oblique', 'normal', 'bold', etc.
+    color = 'Red'  # Puedes usar nombres de colores o códigos hexadecimales
     
     # Usar Markdown y HTML para aplicar estilos al subencabezado
     st.markdown(f'<h3 style="color:{color}; font-style:{font_style};">ANÁLISIS SÍSMICO REGISTRADOS EN EL PERÚ (1960_2022)</h3>', unsafe_allow_html=True)
