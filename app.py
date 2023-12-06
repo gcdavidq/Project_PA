307d0178dea7e4dec9727ad2e7b782a1391daac2
Antony16 <136536376+Sh3ccid@users.noreply.github.com>
2023-12-06 06:18:11 -0500

    Update app.py
    Signed-off-by: Antony16 <136536376+Sh3ccid@users.noreply.github.com>

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ st.markdown(page_bg_img, unsafe_allow_html=True)
 image1 = Image.open('Img_3.jpeg')
 
 # Añadimos un panel de control
-tab1, tab2, tab3 = st.tabs(["_Streamlit_ is :blue[_Inicio_] :sunglasses:", "Análisis a nivel nacional", "Anális a nivel departamental"])
+tab1, tab2, tab3 = st.tabs(["INICIO", "Análisis a nivel nacional", "Anális a nivel departamental"])
 
 with tab1:
     font_style = 'font-weight: bolder;'
