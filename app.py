0c3d78f8662d58646dbc46560928a4a5e11cd279
Antony16 <136536376+Sh3ccid@users.noreply.github.com>
2023-12-05 23:39:11 -0500

    Update app.py
    Signed-off-by: Antony16 <136536376+Sh3ccid@users.noreply.github.com>

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ page_bg_video = """
     </style>
 
     <video autoplay loop muted playsinline>
-        <source src="https://pin.it/3KX5pf7/p1.mp4" type="video/mp4">
+        <source src="https://youtu.be/2TVuhNCoOAs?si=bUQDL2hfiwNSdu3l/p1.mp4" type="video/mp4">
         Tu navegador no soporta el elemento de video.
     </video>
 """
