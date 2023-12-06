3d6f1fcd438d5944098221cf2e23258a1a7f1bb9
Antony16 <136536376+Sh3ccid@users.noreply.github.com>
2023-12-06 00:10:58 -0500

    Update analisis_departamental.py
    Signed-off-by: Antony16 <136536376+Sh3ccid@users.noreply.github.com>

--- analisis_departamental.py
+++ analisis_departamental.py
@@ -13,7 +13,6 @@ def load_department_boundaries():
 @st.cache_data
 def load_data(file):
     data = pd.read_csv(file)
-    # Rename 'FECHA_UTC' to 'Año'
     data['Año'] = data['FECHA_UTC'].astype(str).str[:4]
     data['Mes'] = data['FECHA_UTC'].astype(str).str[4:6]
     data['Día'] = data['FECHA_UTC'].astype(str).str[6:]
@@ -38,7 +37,7 @@ def create_map(data):
         data['normalized_depth'] = data['PROFUNDIDAD'] / data['PROFUNDIDAD'].max()
 
         # Mapa de puntos
-        m = folium.Map(location=[-9.1900, -75.0152], zoom_start=6)
+        m = folium.Map(location=[-9.1900, -75.0152], zoom_start=5)
         for index, row in data.iterrows():
             # Calcular el color en función de la magnitud y profundidad
             color = f'rgba(0, 0, 0, {row["normalized_magnitude"] * 0.8})'
@@ -47,7 +46,7 @@ def create_map(data):
             # Asignar color oscuro a cada departamento
             color = department_color_dict[row['DEPARTAMENTOS']]
 
-            # Crear el círculo con información de magnitud y profundidad en el tooltip
+            # Crear el círculo con información de magnitud y profundidad en el mapa
             folium.CircleMarker(
                 location=[row['LATITUD'], row['LONGITUD']],
                 radius=10,
@@ -65,14 +64,9 @@ def create_map(data):
                      labels={'DEPARTAMENTOS': 'Departamento', 'count': 'Cantidad'},
                      color_discrete_map=department_color_dict)
 
-        # Ajustar dinámicamente el dtick basado en la cantidad de datos
         dtick_value = max(1, int(len(data) / 10))
         fig.update_layout(xaxis_title='Departamento', yaxis_title='Cantidad de Sismos', yaxis=dict(dtick=dtick_value))
         st.plotly_chart(fig)
-
-    else:
-        st.info("No hay datos disponibles para mostrar en el mapa y el gráfico de barras.")
-
 def show_departments_count(data):
     st.subheader('Mapa Sísmico del Perú periodo 1960-2022 (IGP) ')
 
@@ -80,41 +74,53 @@ def show_departments_count(data):
     options = ['DEPARTAMENTOS']
 
     selected_column = st.selectbox('Selecciona una columna:', options)
-
-    # Nueva lógica para la selección de valores (todos o específicos)
     all_values_option = 'Todos'
     unique_values = data[selected_column].unique()
-    selected_values = st.multiselect(f'Selecciona valor(es) de {selected_column}:',
-                                     options=[all_values_option] + list(unique_values),
-                                     default=[all_values_option])
 
-    # Verificar si la opción 'Todos' está seleccionada de forma independiente
+    # Establecer el primer departamento como seleccionado por defecto
+    default_values = [unique_values[0]] if len(unique_values) > 0 else [all_values_option]
+
+    selected_values = st.multiselect(f'Selecciona el (los) DEPARTAMENTO(OS):',
+                                     options=[all_values_option] + list(unique_values),
+                                     default=default_values)
+
     select_all = all_values_option in selected_values
     if select_all:
-        selected_values = [all_values_option]  # Restablecer la selección para incluir todos los valores
-
-    # Filtrar data en función de la selección
-    if select_all:
-        filtered_data = data
+        st.write(f'Se muestran todos los sismos de todos los departamentos.')
+        print("Data (Todos):", data)
+        create_map(data)
+    elif not selected_values:
+        st.warning("No se han seleccionado departamentos. ")
+        ##Creamos un mapa vació con enfoque a Perú.
+        peru_center_coords = [-9.1900, -75.0152]
+        peru_map = folium.Map(location=peru_center_coords, zoom_start=4.8)
+        st.components.v1.html(peru_map._repr_html_(), width=800, height=600, scrolling=True)
     else:
+        # Hay departamentos seleccionados
         filtered_data = data[data[selected_column].isin(selected_values)]
+        print("Filtered Data:", filtered_data)
 
+        min_year = filtered_data['Año'].astype(int).min()
+        max_year = filtered_data['Año'].astype(int).max()
 
-    # Código restante para el slider y el mapa
-    min_year = filtered_data['Año'].astype(int).min()
-    max_year = filtered_data['Año'].astype(int).max()
+        st.markdown("**SELECCIONE EL RANGO DE AÑOS EN EL QUE DESEA VER LAS TABLAS "
+                    "Y EL GRÁFICO DE PROFUNDIDAD**")
+        selected_years = st.slider('Seleccione:',
+                                   min_value=min_year,
+                                   max_value=max_year,
+                                   value=(min_year, max_year),
+                                   key="select_slider_years")
 
-    st.text("SELECCIONE EL RANGO DE AÑOS EN EL QUE DESEA VER LAS TABLAS Y EL GRAFICO DE PROFUNDIDAD")
-    selected_years = st.slider('Seleccione:',
-                               min_value=min_year,
-                               max_value=max_year,
-                               value=(min_year, max_year),
-                               key="select_slider_years")
+        # Aplicar el filtro de año
+        filtered_data = filtered_data[(filtered_data['Año'].astype(int) >= selected_years[0]) &
+                                      (filtered_data['Año'].astype(int) <= selected_years[1])]
 
-    # Aplicar el filtro de año
-    filtered_data = filtered_data[(filtered_data['Año'].astype(int) >= selected_years[0]) &
-                                  (filtered_data['Año'].astype(int) <= selected_years[1])]
+        if len(selected_values) > 0:
+            department_count = filtered_data.shape[0]
+            ## Mostrar mensajes de acuerdo a los departamentos seleccionados
+            if len(selected_values) == 1:
+                st.write(f'En el departamento de {selected_values[0]} hay {department_count} punto{"s" if department_count > 1 else ""} para los años {selected_years[0]} - {selected_years[1]}.')
+            else:
+                st.write(f'En los departamentos seleccionados hay {department_count} puntos para los años {selected_years[0]} - {selected_years[1]}.')
 
-    department_count = filtered_data.shape[0]
-    st.write(f'En {selected_column} hay {department_count} puntos para los años {selected_years[0]} - {selected_years[1]}.')
-    create_map(filtered_data)
+            create_map(filtered_data)
